5f9124917f8a40a6ded07eaa892e55488a07781e
xutian <xutian0521@qq.com>
2019-03-11 09:57:20 +0800

    init project

---
+++ src/module/app.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <img src="../images/logo.png"  alt="logo"/>
+        <span>{{msg}}</span>
+        <p class="test" test text></p>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name:'app2',
+    data(){
+        return {
+            msg:"hello vue~~##"
+        }
+    }
+})
+</script>
+<style>
+    .test{
+        color: darkgreen;
+    }
+</style>
+
